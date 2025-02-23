1e7ac8d67f472b18cd88679d353783785a742168
nghia <nghiabn21@gmail.com>
2025-02-23 17:23:28 +0700

    Tạo dự án ban đầu

---
+++ src/main/java/com/GoogleSheetAPI/GoogleSheetApiApplication.java
@@ -0,0 +1,13 @@
+package com.GoogleSheetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSheetApiApplication.class, args);
+	}
+
+}
